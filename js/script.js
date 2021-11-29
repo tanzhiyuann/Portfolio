ca0038db46cf5f52414bd550e16926b363e90c14
NotAddison <S10222525@connect.np.edu.sg>
2021-11-29 14:43:44 +0800

    Increased height where navbar becomes sticky

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // >> NAVIGATION (NAV) BAR <<
 document.addEventListener("scroll",function(){
     const navbar = document.querySelector(".navbar");
-    const navbarHeight = 10;
+    const navbarHeight = 20;
     const distTop = Math.abs(document.body.getBoundingClientRect().top);
     
     if (distTop >= navbarHeight) navbar.classList.add("sticky");
