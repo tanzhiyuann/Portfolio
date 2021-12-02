dd42f238a2ea19187336899f58d45ca18a237bb6
NotAddison <s10222525@connect.np.edu.sg>
2021-12-03 02:41:52 +0800

    Fix validator error (missing semicolen)

--- js/script.js
+++ js/script.js
@@ -21,12 +21,12 @@ burgor.addEventListener("click", function(){
     if (i%2 === 0){
         console.log('menu closed');
         menu.style.left = "-100vw"; //Moves menu out of visible document first
-        setTimeout(function(){menu.classList.remove("open");},200) //Hides menu after 2 seconds. (display = "none";)
+        setTimeout(function(){menu.classList.remove("open");},200); //Hides menu after 2 seconds. (display = "none";)
     }
     else{ 
         console.log('menu open');
-        menu.classList.add("open"); //Displays menu first (inintial position = -100vw (ouside of visible document))
-        setTimeout(function(){menu.style.left = "0vw";},200) //Moves the menu to position 0, (Basically slides in because of transition in CSS)
+        menu.classList.add("open"); //Displays menu first (initial position = -100vw (outside of visible document))
+        setTimeout(function(){menu.style.left = "0vw";},200); //Moves the menu to position 0, (Basically slides in because of transition in CSS)
         
     }
 });
